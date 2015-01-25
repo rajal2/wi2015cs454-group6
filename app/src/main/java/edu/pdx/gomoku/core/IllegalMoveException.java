738da42bb351034ad5a64dcb50fb34c15062bb31
Biwei <biweiguo@gmail.com>
2015-01-24 22:49:45 -0800

    implemented detection for tie. fixed a illegalMoveExcetion typo to illegalMoveException throughout the project. Need corresponding implementation for tie in GameActivity and xml files.

--- app/src/main/java/edu/pdx/gomoku/core/IllegalMoveException.java
+++ app/src/main/java/edu/pdx/gomoku/core/IllegalMoveException.java
@@ -3,5 +3,5 @@ package edu.pdx.gomoku.core;
 /**
  * Created by yuriy on 1/20/15.
  */
-public class IllegalMoveExcetion extends  Exception {
+public class IllegalMoveException extends  Exception {
 }
